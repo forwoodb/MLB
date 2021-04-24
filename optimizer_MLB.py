21127845eca803a925bbb6c216ebcd07c9c55898
Brent Forwood <forwoodb77@gmail.com>
2021-04-24 14:36:40 -0400

    Fixed Name matchups among sources of data in batter projections

--- optimizer_MLB.py
+++ optimizer_MLB.py
@@ -3,13 +3,17 @@ import pandas as pd
 
 # import csv data
 data = pd.read_csv('./Data/DKSalaries.csv')
-starting_lineups = pd.read_csv('./Data/Lineups_2021_04_23.csv')
+starting_lineups = pd.read_csv('./Data/Lineups_2021_04_24.csv')
 name_spelling = pd.read_csv('./Data/name_spelling.csv')
+projections = pd.read_csv('./projections.csv')
 
 # convert to dataframe
 df_dk = pd.DataFrame(data)
 df_lineups = pd.DataFrame(starting_lineups)
 df_name_spelling = pd.DataFrame(name_spelling)
+df_projections = pd.DataFrame(projections)
+df_projections = df_projections[['Name','pj_vP']]
+
 #re-organize data
 new = df_dk['Roster Position'].str.split('/', n=1, expand=True)
 
@@ -21,133 +25,136 @@ df_lineups = df_lineups.rename(columns={' player name': 'Name'})
 df_lineups = df_lineups.replace(list(df_name_spelling["BaseballMonster"]), list(df_name_spelling["DraftKings"]))
 
 availables = pd.merge(df_lineups, df_dk, on='Name', how='inner')
-print(availables)
-# pitcher = availables[(availables['position_1'] == 'P') | (availables['position_2'] == 'P')]
-# catcher = availables[(availables['position_1'] == 'C') | (availables['position_2'] == 'C')]
-# first = availables[(availables['position_1'] == '1B') | (availables['position_2'] == '1B')]
-# second = availables[(availables['position_1'] == '2B') | (availables['position_2'] == '2B')]
-# third = availables[(availables['position_1'] == '3B') |(availables['position_2'] == '3B') ]
-# short = availables[(availables['position_1'] == 'SS') | (availables['position_2'] == 'SS')]
-# outfield = availables[(availables['position_1'] == 'OF') | (availables['position_2'] == 'OF')]
-#
-# # Lists
-# # positions = ['P', 'C', '1B', '2B', '3B', 'SS', 'OF']
-# positions = 10
-# available_players = list(availables['Name'])
-# # num_available = list(availables[' game_number'])
+availables = pd.merge(df_projections, availables, on='Name', how='inner')
+
+
+pitcher = availables[(availables['position_1'] == 'P') | (availables['position_2'] == 'P')]
+catcher = availables[(availables['position_1'] == 'C') | (availables['position_2'] == 'C')]
+first = availables[(availables['position_1'] == '1B') | (availables['position_2'] == '1B')]
+second = availables[(availables['position_1'] == '2B') | (availables['position_2'] == '2B')]
+third = availables[(availables['position_1'] == '3B') |(availables['position_2'] == '3B') ]
+short = availables[(availables['position_1'] == 'SS') | (availables['position_2'] == 'SS')]
+outfield = availables[(availables['position_1'] == 'OF') | (availables['position_2'] == 'OF')]
+
+# Lists
+# positions = ['P', 'C', '1B', '2B', '3B', 'SS', 'OF']
+positions = 10
+available_players = list(availables['Name'])
+# num_available = list(availables[' game_number'])
 # points = list(availables['AvgPointsPerGame'])
-# salaries = list(availables['Salary'])
-# position_1 = list(availables['position_1'])
-# position_2 = list(availables['position_2'])
-# for v in availables.position_2.unique():
-# 	if v is None:
-# 		v = []
+points = list(availables['pj_vP'])
+salaries = list(availables['Salary'])
+position_1 = list(availables['position_1'])
+position_2 = list(availables['position_2'])
+for v in availables.position_2.unique():
+	if v is None:
+		v = []
+
+# Dictionaries
+# player_num_available = dict(zip(available_players, num_available))
+player_points = dict(zip(available_players, points))
+player_position_1 = dict(zip(available_players, position_1))
+player_position_2 = dict(zip(available_players, position_2))
+player_salary = dict(zip(available_players, salaries))
+# roster_spots = {
+# 	'P': 2,
+# 	'C': 1,
+# 	'1B': 1,
+# 	'2B': 1,
+# 	'3B': 1,
+# 	'SS': 1,
+# 	'OF': 3
+# }
+
+# More Lists
+pitcher = []
+catcher = []
+first = []
+second = []
+third = []
+short = []
+outfield = []
+
+for v in player_position_1:
+	if player_position_1[v] == 'P':
+		pitcher.append(v)
+	elif player_position_1[v] == 'C':
+		catcher.append(v)
+	if player_position_1[v] == '1B':
+		first.append(v)
+	elif player_position_1[v] == '2B':
+		second.append(v)
+	elif player_position_1[v] == '3B':
+		third.append(v)
+	elif player_position_1[v] == 'SS':
+		short.append(v)
+	elif player_position_1[v] == 'OF':
+		outfield.append(v)
+# Set the problem variable
+prob = LpProblem('MLB', LpMaximize)
+
+# Set decision variables
+use_vars = LpVariable.dicts('Start', available_players, 0,1,LpBinary)
+
+# objective function
+SALARY_CAP = 50000
+
+dk_costs =[]
+dk_points = []
+
+dk_costs += lpSum(player_salary[v] * use_vars[v] for v in available_players)
+dk_points += lpSum(player_points[v] * use_vars[v] for v in available_players)
+
+prob += lpSum(dk_points)
+prob += lpSum(dk_costs) <= SALARY_CAP
+
+# constraints
+prob += lpSum(use_vars[v] for v in pitcher) == 2
+prob += lpSum(use_vars[v] for v in catcher) == 1
+prob += lpSum(use_vars[v] for v in first) == 1
+prob += lpSum(use_vars[v] for v in second) == 1
+prob += lpSum(use_vars[v] for v in third) == 1
+prob += lpSum(use_vars[v] for v in short) == 1
+prob += lpSum(use_vars[v] for v in outfield) == 3
+
+print(pitcher)
+
+prob.solve()
+print("Status: ", LpStatus[prob.status])
+print(value(prob.objective))
+
+
+# TOL = .00001
 #
-# # Dictionaries
-# # player_num_available = dict(zip(available_players, num_available))
-# player_points = dict(zip(available_players, points))
-# player_position_1 = dict(zip(available_players, position_1))
-# player_position_2 = dict(zip(available_players, position_2))
-# player_salary = dict(zip(available_players, salaries))
-# # roster_spots = {
-# # 	'P': 2,
-# # 	'C': 1,
-# # 	'1B': 1,
-# # 	'2B': 1,
-# # 	'3B': 1,
-# # 	'SS': 1,
-# # 	'OF': 3
-# # }
+# # for i in available_players:
+# #     if use_vars[i].varValue > TOL:
+# #         print("Put into lineup", i)
 #
-# # More Lists
-# pitcher = []
-# catcher = []
-# first = []
-# second = []
-# third = []
-# short = []
-# outfield = []
+# for v in prob.variables():
+#     if v.varValue != 0:
+#         print(v.name, "=", v.varValue)
 #
-# for v in player_position_1:
-# 	if player_position_1[v] == 'P':
-# 		pitcher.append(v)
-# 	elif player_position_1[v] == 'C':
-# 		catcher.append(v)
-# 	if player_position_1[v] == '1B':
-# 		first.append(v)
-# 	elif player_position_1[v] == '2B':
-# 		second.append(v)
-# 	elif player_position_1[v] == '3B':
-# 		third.append(v)
-# 	elif player_position_1[v] == 'SS':
-# 		short.append(v)
-# 	elif player_position_1[v] == 'OF':
-# 		outfield.append(v)
-# # Set the problem variable
-# prob = LpProblem('MLB', LpMaximize)
-#
-# # Set decision variables
-# use_vars = LpVariable.dicts('Start', available_players, 0,1,LpBinary)
-#
-# # objective function
-# SALARY_CAP = 50000
-#
-# dk_costs =[]
-# dk_points = []
-#
-# dk_costs += lpSum(player_salary[v] * use_vars[v] for v in available_players)
-# dk_points += lpSum(player_points[v] * use_vars[v] for v in available_players)
-#
-# prob += lpSum(dk_points)
-# prob += lpSum(dk_costs) <= SALARY_CAP
-#
-# # constraints
-# prob += lpSum(use_vars[v] for v in pitcher) == 2
-# prob += lpSum(use_vars[v] for v in catcher) == 1
-# prob += lpSum(use_vars[v] for v in first) == 1
-# prob += lpSum(use_vars[v] for v in second) == 1
-# prob += lpSum(use_vars[v] for v in third) == 1
-# prob += lpSum(use_vars[v] for v in short) == 1
-# prob += lpSum(use_vars[v] for v in outfield) == 3
-#
-# print(pitcher)
-#
-# prob.solve()
-# print("Status: ", LpStatus[prob.status])
-# print(value(prob.objective))
-#
-#
-# # TOL = .00001
-# #
-# # # for i in available_players:
-# # #     if use_vars[i].varValue > TOL:
-# # #         print("Put into lineup", i)
-# #
-# # for v in prob.variables():
-# #     if v.varValue != 0:
-# #         print(v.name, "=", v.varValue)
-# #
-# # print("Projected points", value(prob.objective))
-#
-# def summary(prob):
-#     div = '---------------------------------------\n'
-#     print("Variables:\n")
-#     score = str(prob.objective)
-#     constraints = [str(const) for const in prob.constraints.values()]
-#     for v in prob.variables():
-#         score = score.replace(v.name, str(v.varValue))
-#         constraints = [const.replace(v.name, str(v.varValue)) for const in constraints]
-#         if v.varValue != 0:
-#             print(v.name, "=", v.varValue)
-#     print(div)
-#     print("Constraints:")
-#     for constraint in constraints:
-#         constraint_pretty = " + ".join(re.findall("[0-9\.]*\*1.0", constraint))
-#         if constraint_pretty != "":
-#             print("{} = {}".format(constraint_pretty, eval(constraint_pretty)))
-#     print(div)
-#     print("Score:")
-#     score_pretty = " + ".join(re.findall("[0-9\.]+\*1.0", score))
-#     print("{} = {}".format(score_pretty, eval(score)))
-#
-# summary(prob)
+# print("Projected points", value(prob.objective))
+
+def summary(prob):
+    div = '---------------------------------------\n'
+    print("Variables:\n")
+    score = str(prob.objective)
+    constraints = [str(const) for const in prob.constraints.values()]
+    for v in prob.variables():
+        score = score.replace(v.name, str(v.varValue))
+        constraints = [const.replace(v.name, str(v.varValue)) for const in constraints]
+        if v.varValue != 0:
+            print(v.name, "=", v.varValue)
+    print(div)
+    print("Constraints:")
+    for constraint in constraints:
+        constraint_pretty = " + ".join(re.findall("[0-9\.]*\*1.0", constraint))
+        if constraint_pretty != "":
+            print("{} = {}".format(constraint_pretty, eval(constraint_pretty)))
+    print(div)
+    print("Score:")
+    score_pretty = " + ".join(re.findall("[0-9\.]+\*1.0", score))
+    print("{} = {}".format(score_pretty, eval(score)))
+
+summary(prob)
--- projections.py
+++ projections.py
@@ -5,7 +5,7 @@ import re
 csv_dk = pd.read_csv('./Data/DKSalaries.csv')
 csv_b_stats = pd.read_csv('./Data/br_b_stats.csv')
 csv_p_stats = pd.read_csv('./Data/br_p_stats.csv')
-csv_starting_lineups = pd.read_csv('./Data/Lineups_2021_04_23.csv')
+csv_starting_lineups = pd.read_csv('./Data/Lineups_2021_04_24.csv')
 csv_name_spelling = pd.read_csv('./Data/name_spelling.csv')
 csv_team_abbr = pd.read_csv('./Data/team_abb.csv')
 
@@ -16,6 +16,7 @@ df_lineups = pd.DataFrame(csv_starting_lineups)
 df_name_spelling = pd.DataFrame(csv_name_spelling)
 df_team_abbr = pd.DataFrame(csv_team_abbr)
 
+
 df_lineups = df_lineups.rename(columns={' player name': 'Name'})
 df_lineups = df_lineups.rename(columns={' batting order': ' b/o'})
 df_lineups = df_lineups.rename(columns={'team code': 'team'})
@@ -25,7 +26,7 @@ df_lineups = df_lineups.replace(list(df_team_abbr["BaseballMonster"]), list(df_t
 
 # # Batting Stats
 df_b_stats.drop(df_b_stats.tail(1).index,inplace=True)
-name = df_b_stats['Name'].str.split('*', n=1, expand=True)
+name = df_b_stats['Name'].str.split('\*|\#', n=1, expand=True)
 name = name[0].str.split('\\', n=1, expand=True)
 df_b_stats['Name'] = name[0]
 
@@ -33,10 +34,12 @@ df_dk = df_dk[['Name','Game Info','TeamAbbrev','AvgPointsPerGame']]
 df_dk = df_dk.rename(columns={'TeamAbbrev': 'TmAbb','AvgPointsPerGame': 'APPG'})
 df_lineups = df_lineups[['team','Name',' b/o']]
 df_b_stats = df_b_stats[['Name','G','H','2B','3B','HR','BB','R','RBI','HBP','SB']]
-#
+
+df_b_stats = df_b_stats.replace(list(df_team_abbr["BaseballReference"]), list(df_team_abbr["DraftKings"]))
+df_b_stats = df_b_stats.replace(list(df_name_spelling["BaseballReference"]), list(df_name_spelling["DraftKings"]))
+
 df_dk = pd.merge(df_lineups, df_dk, on='Name', how='inner')
 df_dk = pd.merge(df_dk, df_b_stats, on='Name', how='inner')
-
 
 # Batting Stats
 singles = ((df_dk['H']-df_dk['2B']-df_dk['3B']-df_dk['HR']) * 3)/df_dk['G']
@@ -61,21 +64,16 @@ df_dk['SB'] = round(stolen_bases, 2)
 
 proj_pts = (singles + doubles + triples + home_runs + RBIs + runs + walks + HBP + stolen_bases)
 df_dk['pj_pts'] = round(proj_pts, 2)
-# dk_batting_stats.sort_values(by=['proj_pts'], inplace=True, ascending=False)
-
-
-
 
 # # Starting Pitcher Factors for Batting
 df_p_stats.drop(df_p_stats.tail(1).index,inplace=True)
 name = df_p_stats['Name'].str.split('*', n=1, expand=True)
 name = name[0].str.split('\\', n=1, expand=True)
 df_p_stats['Name'] = name[0]
-#
+
 df_p_stats = df_p_stats[['Name','IP','H','HR','R','BB']]
-#
+
 lg_innings_pitched = df_p_stats['IP'].sum()
-# lg_hits_allowed = df_p_stats['H'].sum()
 
 lg_hits_allowed = df_p_stats['H'].sum() - df_p_stats['HR'].sum()
 lg_hits_ip = lg_hits_allowed/lg_innings_pitched
@@ -102,11 +100,10 @@ bb_fac = bb_ip/lg_bb_ip
 df_p_stats.insert(9, 'bb_fac', round(bb_fac, 2))
 
 #Starting Pitcher Stats
-
 game_info = df_dk['Game Info'].str.split('@', n=1, expand=True)
 df_dk['team_1'] = game_info[0]
 df_dk['team_2'] = game_info[1]
-
+#
 team_2 = df_dk['team_2'].str.split(' ', n=1, expand=True)
 df_dk['team_2'] = team_2[0]
 df_dk.drop(columns=['Game Info'], inplace=True)
@@ -118,26 +115,26 @@ for i in df_dk.index:
         df_dk['Opp'][i] = df_dk['team_2'][i]
     else:
         df_dk['Opp'][i] = df_dk['team_1'][i]
-#
+
 starting_pitchers = {}
 for pos in df_dk[' b/o'].unique():
     if pos == 'SP':
         availables_sp = df_dk[df_dk[' b/o'] == pos]
         starting_pitchers = list(availables_sp[['TmAbb', 'Name']].set_index('TmAbb').to_dict().values())[0]
-#
-# # print(starting_pitchers)
+
+pd.set_option('display.max_rows', None)
+print(df_dk)
+
 df_dk['vSP'] = None
 #
 for i in df_dk.index:
     for j in starting_pitchers:
         if df_dk['Opp'][i] == j:
             df_dk['vSP'][i] = starting_pitchers[j]
-#
+
 df_p_stats = df_p_stats.rename(columns={'Name': 'vSP'})
 df_dk.drop(columns=['team_1','team_2'], inplace=True)
 
-# df_dk = pd.merge(df_dk, df_b_stats, on='Name', how='inner')
-# df_p_stats = df_p_stats[['vSP','H','h_fac','HR','hr_fac','R','r_fac','BB','bb_fac']]
 df_p_stats = df_p_stats[['vSP','h_fac','hr_fac','r_fac','bb_fac']]
 df_dk = pd.merge(df_dk, df_p_stats, on='vSP', how='inner')
 
@@ -147,15 +144,14 @@ proj_hr = df_dk['HR'] * df_dk['hr_fac']
 proj_r = (df_dk['R'] + df_dk['RBI']) * df_dk['r_fac']
 proj_bb = df_dk['BB'] * df_dk['bb_fac']
 
-# proj_pts_vP = HBP
 proj_pts_vP = proj_hits + proj_hr + proj_r + proj_bb + df_dk['HBP'] + df_dk['SB']
 
 df_dk['pj_vP'] = round(proj_pts_vP, 2)
-# df_dk.sort_values(by=['proj_pts_vP'], inplace=True, ascending=False)
-# display entire dataframe in console
-# projections = df_dk[['Name','TeamAbbrev','Opp','vSP','AvgPointsPerGame','proj_pts_vP']]
-#
-# projection_stats = df_b_stats[['Name','']]
+
 pd.set_option('display.max_rows', None)
 # pd.set_option('display.max_columns', None)
 print(df_dk)
+
+# * Two players named Will Smith produces doubles
+
+df_dk.to_csv('./projections.csv')
