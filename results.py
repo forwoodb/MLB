93e5dbff4ac41505b48013ad08ec09dfceff3a5a
Brent Forwood <forwoodb77@gmail.com>
2021-05-02 17:16:32 -0400

    Fixed clean names function for results lineups, removed pitchers' batting stats from table

--- results.py
+++ results.py
@@ -10,21 +10,18 @@ name_spelling = pd.read_csv('./Spelling/name_spelling.csv')
 
 # Import csv data.
 csv_lineups = pd.read_csv('./lineups.csv')
-csv_stats_batter = pd.read_csv('./Data/' + date + '/FanGraphs Leaderboard bat 4-28-21.csv')
+csv_stats_batter = pd.read_csv('./Data/' + date + '/FanGraphs Leaderboard bat.csv')
 
 # Convert csv data to dataframe.
 df_lineups = pd.DataFrame(csv_lineups)
 df_stats_batter = pd.DataFrame(csv_stats_batter)
 
-# Get DraftKings score for players.
 def clean_names(num):
-    new = df_lineups[num].str.split('_', n=2, expand=True)
-    df_lineups['first'] = new[1]
-    df_lineups['last'] = new[2]
-    df_lineups.drop(columns=[num], inplace=True)
-    df_lineups['player' + num] = df_lineups['first'] + ' ' + df_lineups['last']
-    df_lineups.drop(columns=['first'], inplace=True)
-    df_lineups.drop(columns=['last'], inplace=True)
+    for item in df_lineups[num]:
+        name = item.split('_')
+        name.pop(0)
+        name = ' '.join(name)
+        df_lineups[num].replace({item: name}, inplace=True)
 
 clean_names('1')
 clean_names('2')
@@ -50,4 +47,41 @@ df_stats_batter['Total'] = df_stats_batter['1B'] + df_stats_batter['2B'] + df_st
 
 df_totals = df_stats_batter[['Name', 'Total']]
 
-print(df_totals)
+def get_scores(num):
+    pd.merge(df_lineups[num], df_totals, left_on=num, right_on='Name', on=num, how='inner')
+
+get_scores('1')
+# totals = dict(zip(df_stats_batter['Name'], df_stats_batter['Total']))
+
+
+# df_lineups = df_lineups.T
+# df_lineups.columns=df_lineups.iloc[1]
+# df_lineups = df_lineups.reindex(df_lineups.index.drop('Lineup Type'))
+
+# df_roster = df_lineups[['1','2','3','4','5','6','7','8','9','10']]
+# rosters = df_roster.values.tolist()
+
+# for list in rosters:
+#     print(list)
+#
+# for k in rosters[0]:
+#     if k in totals:
+#         print(k, totals[k])
+
+# df_pivot_lineups = df_lineups.pivot_table(index='')
+# scores = pd.merge(df_lineups, df_totals)
+# scores = pd.merge(df_lineups, df_totals, left_on='APPG', right_on='Name')
+# scores = pd.merge(df_lineups, df_totals, left_on='pj_vO', right_on='Name')
+# scores = pd.merge(df_lineups, df_totals, left_on='3', right_on='Name')
+# scores = pd.merge(df_lineups, df_totals, left_on='4', right_on='Name')
+# scores = pd.merge(df_lineups, df_totals, left_on='5', right_on='Name')
+# scores = pd.merge(df_lineups, df_totals, left_on='6', right_on='Name')
+# scores = pd.merge(df_lineups, df_totals, left_on='7', right_on='Name')
+# scores = pd.merge(df_lineups, df_totals, left_on='8', right_on='Name')
+# scores = pd.merge(df_lineups, df_totals, left_on='9', right_on='Name')
+# scores = pd.merge(df_lineups, df_totals, left_on='10', right_on='Name')
+# scores = pd.merge(df_lineups, df_stats_batter, on='Name', how='inner')
+
+pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_columns', None)
+print(df_lineups)
